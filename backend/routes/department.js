a15119a4cd9e2afdd80aa741d261abfdfdf2c8be
Onoja <onojamatthew59@gmail.com>
2019-09-16 21:39:33 +0100

    new update

--- backend/routes/department.js
+++ backend/routes/department.js
@@ -4,6 +4,6 @@ const { postEmployee } = require("../controller/employee");
 const router = express.Router();
 
 router.post("/employee", postEmployee);
-router.post("/employee/:employeeId", postEmployee);
+// router.post("/employee/:employeeId", getEmployee);
 
 module.exports = router;
